93e3f4f0916b2ba0ab4a6cdda2e493ff1d47fb3a
= <=>
2018-09-10 00:10:15 -0400

    finish

--- src/components/CoordinatesButton.js
+++ src/components/CoordinatesButton.js
@@ -1,1 +1,10 @@
-// Code CoordinatesButton Component Here
+import React from 'react'
+
+export default props => {
+  const handleClick = e => {
+    e.persist()
+    props.onReceiveCoordinates([e.clientX, e.clientY])
+  }
+
+  return <button onClick={handleClick}>Button</button>
+}
